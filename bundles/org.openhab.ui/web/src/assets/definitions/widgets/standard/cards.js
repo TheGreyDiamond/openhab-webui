2d45e02af05b0de588a7d627b7130a6ef7cd8b22
TheGreyDiamond <s-einkauf@oest-rs.de>
2021-08-07 19:41:28 +0200

    changed some lines

--- bundles/org.openhab.ui/web/src/assets/definitions/widgets/standard/cards.js
+++ bundles/org.openhab.ui/web/src/assets/definitions/widgets/standard/cards.js
@@ -69,7 +69,6 @@ export const OhRollershutterCardDefinition = () => new WidgetDefinition('oh-roll
   ])
   .paramGroup(pg('controls', 'Rollershutter Controls'), RollershutterParameters())
 
-
 // OhSliderCard
 import SliderParameters from '../system/slider.js'
 export const OhSliderCardDefinition = () => new WidgetDefinition('oh-slider-card', 'Slider Card', 'Display a slider in a card to control an item')
@@ -164,4 +163,3 @@ export const OhClockCardDefinition = () => new WidgetDefinition('oh-clock-card',
       })
   ])
   .paramGroup(actionGroup(null, 'Action to perform when the clock is clicked'), actionParams())
-
